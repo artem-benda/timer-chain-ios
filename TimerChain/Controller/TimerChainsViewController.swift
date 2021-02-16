fba0e96ba1cdba99580c0d98d0c95ff4eb608122
Artem Benda <abenda@inbox.ru>
2021-02-16 19:05:59 +0300

    intermediate commit, TimerChainsViewController

--- TimerChain/Controller/TimerChainsViewController.swift
+++ TimerChain/Controller/TimerChainsViewController.swift
@@ -6,13 +6,214 @@
 //
 
 import UIKit
+import CoreData
 
 class TimerChainsViewController: UIViewController {
-
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var collectionViewOperations: [BlockOperation] = []
+    
+    var dataController: DataController!
+    var fetchedResultController: NSFetchedResultsController<Chain>!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let columnLayout = ColumnFlowLayout(
+                    cellsPerRow: 1,
+                    minimumInteritemSpacing: 10,
+                    minimumLineSpacing: 10,
+                    sectionInset: UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+                )
+        collectionView.setCollectionViewLayout(columnLayout, animated: true)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setupFetchedResultController()
+
+        if let indexPaths = collectionView.indexPathsForSelectedItems, !indexPaths.isEmpty {
+            indexPaths.forEach { (indexPath) in
+                collectionView.deselectItem(at: indexPath, animated: true)
+            }
+            collectionView.reloadItems(at: indexPaths)
+        }
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        fetchedResultController = nil
+    }
+    
+    private func setupFetchedResultController() {
+        let fetchRequest: NSFetchRequest<Chain> = Chain.fetchRequest()
+        let sortDescriptor = NSSortDescriptor(key: "orderIndex", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        if let searchText = searchBar.text {
+            let predicate = NSPredicate(format: "(name CONTAINS[cd] %@)", searchText)
+            fetchRequest.predicate = predicate
+        }
+        
+        NSFetchedResultsController<Chain>.deleteCache(withName: "chains")
+        fetchedResultController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: dataController.viewContext, sectionNameKeyPath: nil, cacheName: "chains")
+        fetchedResultController.delegate = self
+        
+        do {
+            try fetchedResultController.performFetch()
+        } catch {
+            fatalError("The fetch could not be performed: \(error.localizedDescription)")
+        }
+    }
+    
+    /// Display an alert prompting the user to name a new timer chain. Calls
+    /// `addChain(name:)`.
+    func presentNewChainAlert() {
+        let alert = UIAlertController(title: "New Timer Chain", message: "Enter a name for this Chain", preferredStyle: .alert)
+
+        // Create actions
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        let saveAction = UIAlertAction(title: "Save", style: .default) { [weak self] action in
+            if let name = alert.textFields?.first?.text {
+                self?.addChain(name: name)
+            }
+        }
+        saveAction.isEnabled = false
+
+        // Add a text field
+        alert.addTextField { textField in
+            textField.placeholder = "Name"
+            NotificationCenter.default.addObserver(forName: UITextField.textDidChangeNotification, object: textField, queue: .main) { notif in
+                if let text = textField.text, !text.isEmpty {
+                    saveAction.isEnabled = true
+                } else {
+                    saveAction.isEnabled = false
+                }
+            }
+        }
+
+        alert.addAction(cancelAction)
+        alert.addAction(saveAction)
+        present(alert, animated: true, completion: nil)
+    }
+
+    /// Adds a new timer chain to the database
+    func addChain(name: String) {
+        // Returning if the request is not performed
+        guard let fetchedObjects = fetchedResultController.fetchedObjects else { return }
+        
+        let chain = Chain(context: dataController.viewContext)
+        chain.name = name
+        chain.createdAt = Date()
+        // Set orderIndex, so it would be last element
+        chain.orderIndex = Int64(fetchedObjects.count)
+        try? dataController.viewContext.save()
+    }
+
+    /// Deletes the timer chain at the specified index path
+    func deleteChain(at indexPath: IndexPath) {
+        let chainToDelete = fetchedResultController.object(at: indexPath)
+        dataController.viewContext.delete(chainToDelete)
+        try? dataController.viewContext.save()
+    }
+    
+    deinit {
+        for o in collectionViewOperations { o.cancel() }
+        collectionViewOperations.removeAll()
     }
 
 }
 
+extension TimerChainsViewController: UICollectionViewDelegate {
+}
+
+extension TimerChainsViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return fetchedResultController.sections?[section].numberOfObjects ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "timerChainsCell", for: indexPath) as! TimerChainsCollectionViewListCell
+        // TODO set values for cell
+        return cell
+    }
+}
+
+extension TimerChainsViewController: UICollectionViewDragDelegate {
+    func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+        // TODO
+        guard let fetchedObjects = fetchedResultController.fetchedObjects else { return [UIDragItem]() }
+        
+        let chain = fetchedObjects[indexPath.row]
+        let itemProvider = NSItemProvider(object: chain.objectID.uriRepresentation().absoluteString as NSString)
+        let dragItem = UIDragItem(itemProvider: itemProvider)
+        dragItem.localObject = chain
+        return [dragItem]
+    }
+}
+
+extension TimerChainsViewController: UICollectionViewDropDelegate {
+    func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: UICollectionViewDropCoordinator) {
+        guard let fetchedObjects = fetchedResultController.fetchedObjects else { return }
+        let destinationIndexPath = IndexPath(item: collectionView.numberOfItems(inSection: 0), section: 0)
+        let dropItem = coordinator.items[0]
+        let chain = dropItem.dragItem.localObject as! Chain
+        switch coordinator.proposal.operation {
+        case .move, .copy:
+            _ = dropItem.dragItem.itemProvider.loadObject(ofClass: NSString.self, completionHandler: { (string, error) in
+                if error == nil, let  {
+                    
+                }
+            })
+        default: break
+        }
+    }
+}
+
+extension TimerChainsViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        setupFetchedResultController()
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = true
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.endEditing(true)
+    }
+}
+
+extension TimerChainsViewController: NSFetchedResultsControllerDelegate {
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+            case .insert:
+                collectionViewOperations.append(BlockOperation(block: { [weak self] in
+                    self!.collectionView.insertItems(at: [newIndexPath!])
+                }))
+            case .delete:
+                collectionViewOperations.append(BlockOperation(block: { [weak self] in
+                    self!.collectionView.deleteItems(at: [indexPath!])
+                }))
+            case .update:
+                collectionViewOperations.append(BlockOperation(block: { [weak self] in
+                    self!.collectionView.reloadItems(at: [indexPath!])
+                }))
+            case .move:
+                collectionViewOperations.append(BlockOperation(block: { [weak self] in
+                    self!.collectionView.moveItem(at: indexPath!, to: newIndexPath!)
+                }))
+            @unknown default:
+                break
+        }
+    }
+
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        collectionView.performBatchUpdates({ () -> Void in
+            for op: BlockOperation in self.collectionViewOperations { op.start() }
+        }, completion: { (finished) -> Void in self.collectionViewOperations.removeAll() })
+    }
+}
---
+++ TimerChain/View/ColumnFlowLayout.swift
@@ -0,0 +1,47 @@
+//
+//  ColumnFlowLayout.swift
+//  Meme Me
+//
+//  Created by Artem Benda on 22.12.2020.
+//
+
+import Foundation
+import UIKit
+
+/**
+ * Flow layout with definite column count and square cells
+ * from stackoverflow forum
+ * https://stackoverflow.com/questions/14674986/uicollectionview-set-number-of-columns
+ */
+class ColumnFlowLayout: UICollectionViewFlowLayout {
+
+    let cellsPerRow: Int
+
+    init(cellsPerRow: Int, minimumInteritemSpacing: CGFloat = 0, minimumLineSpacing: CGFloat = 0, sectionInset: UIEdgeInsets = .zero) {
+        self.cellsPerRow = cellsPerRow
+        super.init()
+
+        self.minimumInteritemSpacing = minimumInteritemSpacing
+        self.minimumLineSpacing = minimumLineSpacing
+        self.sectionInset = sectionInset
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func prepare() {
+        super.prepare()
+
+        guard let collectionView = collectionView else { return }
+        let marginsAndInsets = sectionInset.left + sectionInset.right + collectionView.safeAreaInsets.left + collectionView.safeAreaInsets.right + minimumInteritemSpacing * CGFloat(cellsPerRow - 1)
+        let itemWidth = ((collectionView.bounds.size.width - marginsAndInsets) / CGFloat(cellsPerRow)).rounded(.down)
+        itemSize = CGSize(width: itemWidth, height: itemWidth)
+    }
+
+    override func invalidationContext(forBoundsChange newBounds: CGRect) -> UICollectionViewLayoutInvalidationContext {
+        let context = super.invalidationContext(forBoundsChange: newBounds) as! UICollectionViewFlowLayoutInvalidationContext
+        context.invalidateFlowLayoutDelegateMetrics = newBounds.size != collectionView?.bounds.size
+        return context
+    }
+}
