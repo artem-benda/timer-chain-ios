ab4a5b5c19f9eb0ce834804eda9ac35290c3a93b
Artem Benda <abenda@inbox.ru>
2021-02-19 18:14:31 +0300

    added other view controller stubs

---
+++ TimerChain/Controller/PlayTimerChainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PlayTimerChainViewController.swift
+//  TimerChain
+//
+//  Created by Artem Benda on 19.02.2021.
+//
+
+import UIKit
+
+class PlayTimerChainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ TimerChain/Controller/RecordAudioViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RecordAudioViewController.swift
+//  TimerChain
+//
+//  Created by Artem Benda on 19.02.2021.
+//
+
+import UIKit
+
+class RecordAudioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
